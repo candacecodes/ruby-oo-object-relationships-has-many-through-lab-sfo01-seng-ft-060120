7e16b336b1b81e650a0d5fd50fe5ef9d030ab26d
eager-class-5814 <candacehchoe@gmail.com>
2020-06-08 20:11:33 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Patient 
+  attr_reader :name 
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self  
+  end 
+  
+  def new_appointment(doctor, date)
+    Appointment.new(doctor, self, date)
+  end 
+  
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self} 
+  end 
+  
+  def doctors 
+    appointments.map do |appt|
+      appointments.doctor 
+    end 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
+end 
